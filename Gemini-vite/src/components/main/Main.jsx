7786115add619022a2d5228e3443692b08d34905
SantiHerrera18 <santi.herrera318@gmail.com>
2025-07-23 00:20:24 -0500

    IA interaction

--- Gemini-vite/src/components/main/Main.jsx
+++ Gemini-vite/src/components/main/Main.jsx
@@ -4,7 +4,15 @@ import "./main.css";
 import { AIContext } from "../../context/AIContext";
 
 const Main = () => {
-  const { requestAI } = useContext(AIContext);
+  const {
+    requestAI,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(AIContext);
 
   return (
     <div className="main">
@@ -13,43 +21,64 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Dev.</span>
-          </p>
-          <p>How can I help today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest me something about trading</p>
-            <img src={assets.compass_icon} alt="" />
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev.</span>
+              </p>
+              <p>How can I help today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest me something about trading</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>I don't even know what to say</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>This is another suggestion</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Improve the readability if the following</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
           </div>
-          <div className="card">
-            <p>I don't even know what to say</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>This is another suggestion</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Improve the readability if the following</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
+        )}
         <div className="main-button">
           <div className="search-box">
-            <input type="text" placeholder="Ask Gemini" />
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Ask Gemini"
+            />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img
-                src={assets.send_icon}
-                alt=""
-                onClick={async () => {
-                  console.log(await requestAI("what is calisthenics"));
-                }}
-              />
+              <img src={assets.send_icon} alt="" onClick={() => requestAI()} />
             </div>
           </div>
           <p className="bottom-info">
--- Gemini-vite/src/context/AIProvider.jsx
+++ Gemini-vite/src/context/AIProvider.jsx
@@ -1,12 +1,41 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import main from "../config/gemini.mjs";
 import { AIContext } from "./AIContext";
 
 export function AIProvider({ children }) {
-  const requestAI = async (text) => {
-    return await main(text);
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPromts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const requestAI = async () => {
+    setResultData("");
+    setRecentPrompt(input);
+    setLoading(true);
+    setShowResult(true);
+    const response = await main(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
+  };
+
+  const contextValue = {
+    requestAI,
+    prevPromts,
+    setPrevPrompts,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
   };
 
   return (
-    <AIContext.Provider value={{ requestAI }}>{children}</AIContext.Provider>
+    <AIContext.Provider value={contextValue}>{children}</AIContext.Provider>
   );
 }
